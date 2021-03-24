e24a3bd8106030cd3a8c52309588c1ce23261a23
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-03-24 14:45:31 +0100

    Create generator for random ExamAttendence objects

--- de/jo3rn/horstlchain/ExamAttendance.java
+++ de/jo3rn/horstlchain/ExamAttendance.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Random;
 
 public class ExamAttendance {
   private int matriculationNumber;
@@ -36,5 +37,21 @@ public class ExamAttendance {
     );
 
     System.out.println(examAttendance);
+  }
+
+  public static ExamAttendance getRandomAttendance() {
+    Random random = new Random();
+
+    int lowerBound = 100000;
+    int upperBound = 9999999;
+    int randomMatriculation = lowerBound + (int) (Math.random() * ((upperBound - lowerBound) + 1));
+
+    String[] modules = new String[]{ "Programmierung 1", "Programmierung 2", "Mathe", "BWL" };
+    String randomModule = modules[random.nextInt(modules.length)];
+
+    double[] grades = new double[]{ 1.0, 1.3, 1.7, 2.0, 2.3, 2.7, 3.0, 3.3, 3.7, 4.0, 5.0 };
+    double randomGrade = grades[random.nextInt(grades.length)];
+
+    return new ExamAttendance(randomMatriculation, randomModule, randomGrade, LocalDate.now());
   }
 }
